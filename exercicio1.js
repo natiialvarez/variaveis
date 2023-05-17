81c0b12e86df93af4cca79bef6e4fd3107ac87ee
Natalia Alvarez <natalia.fsalvarez@gmail.com>
2023-05-17 16:58:58 -0300

    feito o exercicio

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+// fazendo os exercicios propostos
+
+const cidadeNatal = "Salvador"
+console.log(cidadeNatal)
+
+//
+
+let populacao = 2_900_319
+console.log(populacao)
+
+populacao = 2_900_320
+console.log(populacao)
+
+console.log(`A populacao de ${cidadeNatal} é aproximadamente de ${populacao}`)
