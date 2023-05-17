61a273098d7cbb3256d07a2781ebec0543cde48a
Natalia Alvarez <natalia.fsalvarez@gmail.com>
2023-05-17 16:48:16 -0300

    new changes

---
+++ variaveis-number.js
@@ -0,0 +1,11 @@
+//criando variaveis com numeros
+
+const idade = 30
+console.log(idade)
+
+//lembrar de usar . (ponto) ao inves da v, (virgula)
+const alturaEmMetros = 1.61
+console.log(alturaEmMetros)
+
+//string template
+console.log(`Minha idade é ${idade} anos e minha altura é ${alturaEmMetros}`)
---
+++ variaveis.js
@@ -0,0 +1,18 @@
+// criando variaveis e guardando um string
+//const é uma variavel que não pode ser mudada
+
+const primeiroNome = "Natália";
+
+console.log(primeiroNome)
+
+// o let é possivel criar uma variavel que possa ficar mudando
+
+let segundoNome = "Natália"
+console.log(segundoNome)
+
+segundoNome = "Fernanda"
+console.log(segundoNome)
+
+
+// string template
+console.log(`Meu nome é ${primeiroNome} e meu segundo nome é ${segundoNome}`)
