1f12528edcc58d612a92cd333f894850048e1a07
Natalia Alvarez <natalia.fsalvarez@gmail.com>
2023-05-17 20:04:38 -0300

    colocado uma aprte sobre concatenação

--- aritimeticas.js
+++ aritimeticas.js
@@ -1,13 +1,13 @@
 let x = 10, y = 2
 
 let resultado = x + y
-console.log(resultado)
+console.log(`A soma de x + y é ${resultado}`)
 
 resultado = x - y
-console.log(resultado)
+console.log(`A subtração de + - y é ${resultado}`)
 
 resultado = x * y
-console.log(resultado)
+console.log(`A mutiplicação de x * y é ${resultado}`)
 
 resultado = x / y
-console.log(resultado)
+console.log(`A divisão de +/y é ${resultado}`)
---
+++ concatenançao.js
@@ -0,0 +1,7 @@
+// juntando strings
+
+let nome = "Natália"
+let sobrenome = "Alvarez"
+
+let nomeCompleto = nome + " " + sobrenome
+console.log(nomeCompleto)
