c303c9a2b18ef42945fca2bbfc7e69b98188eec0
Natalia Alvarez <natalia.fsalvarez@gmail.com>
2023-05-17 17:17:04 -0300

    tipos de variaveis

---
+++ tipos.js
@@ -0,0 +1,13 @@
+let nome = "Natália" //string
+let idade = 29 //number
+let altura = 1.61 //number
+
+//undefined
+let nota
+console.log(nota)
+
+nota = 10
+console.log(nota)
+//null serve para apagar o valor, setando um valor sem significado ou valor
+nota = null
+console.log(nota)
