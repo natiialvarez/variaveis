1d119c290fb311e36678af63ad07c90aa44622e7
Natalia Alvarez <natalia.fsalvarez@gmail.com>
2023-05-17 20:14:11 -0300

    Feito um string template

--- aritimeticas.js
+++ aritimeticas.js
@@ -2,12 +2,20 @@ let x = 10, y = 2
 
 let resultado = x + y
 console.log(`A soma de x + y é ${resultado}`)
+let mensagem = "a soma de " + x + " e " + y + " é " + resultado
+console.log(mensagem)
 
 resultado = x - y
 console.log(`A subtração de + - y é ${resultado}`)
+mensagem = "A subtração de " + x + " e " + y + " é " + resultado
+console.log(mensagem)
 
 resultado = x * y
 console.log(`A mutiplicação de x * y é ${resultado}`)
+mensagem = "A mutiplicação de " + x + " e " + y + " é " + resultado
+console.log(mensagem)
 
 resultado = x / y
 console.log(`A divisão de +/y é ${resultado}`)
+mensagem = "A divisão de " + x + " e " + y + " é " + resultado
+console.log(mensagem)
