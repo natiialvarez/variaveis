faf2b33825941ad3e31e7b0dc69319ed9f9d2162
Natalia Alvarez <natalia.fsalvarez@gmail.com>
2023-05-17 20:43:47 -0300

    operador modular

---
+++ variaveis-modulo.js
@@ -0,0 +1,7 @@
+//como calcular o resto de uma divisão. esse resto é o valor que sobre em uma divisão, por ex. o valor que sobra de 12/5 é 2!
+
+let resto = 12 % 5
+console.log(resto)
+
+resto = 23 % 5
+console.log(resto)
