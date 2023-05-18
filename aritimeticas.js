d50acd1dae2b2eba7701b875e24239104f2f8dd4
Natalia Alvarez <natalia.fsalvarez@gmail.com>
2023-05-17 21:02:28 -0300

    add sobre potencia e raiz quadrada

--- aritimeticas.js
+++ aritimeticas.js
@@ -34,3 +34,12 @@ console.log(x)
 
 x *= 3
 console.log(x)
+
+//calculando raiz quadrada
+
+let raiz = Math.sqrt(16)
+console.log(raiz)
+
+let potencia = Math.pow(2, 3)
+// para potencia vc pode usar tbm 2 ** 3
+console.log(potencia)
